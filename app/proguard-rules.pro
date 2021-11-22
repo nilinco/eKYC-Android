7b5c9e70f9c49f5cef74ac29fea1e2cccd08a685
AmirHossein <ah.ferdosizadeh@gmail.com>
2021-11-22 16:13:13 +0330

    Add EKYC SDK version 1.4.0

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,7 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+-keep class co.nilin.** { *; }
+-keepclassmembers class ** {
+    @org.greenrobot.eventbus.Subscribe <methods>;
+}
--- app/src/main/java/co/nilin/ekycsdksample/MainActivity.kt
+++ app/src/main/java/co/nilin/ekycsdksample/MainActivity.kt
@@ -1,13 +1,36 @@
 package co.nilin.ekycsdksample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.provider.Settings.Secure
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import co.nilin.sdk.ekyc.EkycSdk
 
 class MainActivity : AppCompatActivity() {
+
+    private val ekycSdk by lazy {
+        EkycSdk.getClient(SERVER, API_KEY, getDeviceId())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startKyc(view: View) {
+        val phoneNumber = "" // ex: 09129876543
+        startActivityForResult(ekycSdk.intent(this, phoneNumber), 100)
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun getDeviceId(): String {
+        return Secure.getString(contentResolver, Secure.ANDROID_ID)
+    }
+
+    companion object {
+        private const val API_KEY = ""
+        private const val SERVER = "https://ekycuat.izbank.ir"
     }
 
 }
